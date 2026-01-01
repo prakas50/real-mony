883f3e7ac6d12031825f34ff1c6e93496d263988
prakas50 <72373501+prakas50@users.noreply.github.com>
2026-01-01 13:04:42 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+{
+  "name": "Aviator Pro",
+  "short_name": "Aviator",
+  "start_url": "/index.html",
+  "display": "standalone",
+  "background_color": "#0d1117",
+  "theme_color": "#0d1117",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/7893/7893979.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/7893/7893979.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
